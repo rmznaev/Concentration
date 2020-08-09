b2720fb34272cf638fcd5359477e4f79bbd07147
Ramazan Abdullayev <ramazan.aev@gmail.com>
2020-08-09 18:40:37 +0400

    added ui and some code

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var emojiChoices = ["👻", "🎃", "👻", "🎃"]
+    
+    var flipCount = 0 {
+        didSet {
+            flipCounLabel.text = "Flips: \(flipCount )"
+        }
     }
-
-
+    
+    @IBOutlet weak var flipCounLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button .currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.6235294118, blue: 0.03921568627, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
-
