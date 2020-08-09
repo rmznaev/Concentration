d139a7c663c4280545a49bf7c29a4fe3c8c83ef0
Ramazan Abdullayev <ramazan.aev@gmail.com>
2020-08-09 16:04:44 +0400

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Ramazan Abdullayev on 8/9/20.
+//  Copyright © 2020 Ramazan Abdullayev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
